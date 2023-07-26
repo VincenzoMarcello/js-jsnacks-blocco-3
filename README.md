a2c89f77a8f587546aeb0ab6bf67ad421f8ca49b
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-07-26 16:06:08 +0200

    feat: add second snack

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TRACCIA 1
+<!-- # TRACCIA 1
 
 Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
 
@@ -6,4 +6,14 @@ Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale com
 
 - chiedo all'utente un numero
 - chiedo all'utente un'altro numero
-- genero un numero casuale
+- genero un numero casuale -->
+
+# TRACCIA 2
+
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+- chiedere una parola
+- chiedere un'altra parola
+- se le due parole sono uguali stampo entrambe
+- altrimenti se la prima parola è più lunga stampo la prima
+- altrimenti la seconda parola è più lunga stampo la seconda
--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-//* chiedo all'utente un numero
-const firstNumber = parseInt(prompt("Inserisci un numero"));
+// //* chiedo all'utente un numero
+// const firstNumber = parseInt(prompt("Inserisci un numero"));
 
-//* chiedo all'utente un'altro numero
-const secondNumber = parseInt(prompt("Inserisci un'altro numero"));
+// //* chiedo all'utente un'altro numero
+// const secondNumber = parseInt(prompt("Inserisci un'altro numero"));
 
-//* genero un numero casuale
-let NumberCompreso = Math.floor(Math.random() * secondNumber) + firstNumber;
+// //* genero un numero casuale
+// if (firstNumber < secondNumber) {
+//   let NumberCompreso =
+//     Math.floor(Math.random() * (firstNumber - secondNumber + 1)) + firstNumber;
+// }
 
-console.log(NumberCompreso);
+// console.log(NumberCompreso);
+
+//* chiedo all'utente una parola
+const primaParola = prompt("Inserisci una parola");
+
+//* chiedo all'utente un'altra parola
+const secondaParola = prompt("Inserisci un'altra parola");
+
+//* se le due parole sono uguali stampo entrambe
+if (primaParola.length == secondaParola.length) {
+  console.log(primaParola, secondaParola);
+}
+
+//* altrimenti se la prima parola è più lunga stampo la prima
+else if (primaParola.length > secondaParola.length) {
+  console.log(primaParola);
+}
+
+//* altrimenti la seconda parola è più lunga stampo la seconda
+else {
+  console.log(secondaParola);
+}
