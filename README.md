2a487469ba5aecff55324d2fe9bedef9dd4d8302
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-07-26 17:31:54 +0200

    feat: add all exercise

--- README.md
+++ README.md
@@ -8,12 +8,40 @@ Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale com
 - chiedo all'utente un'altro numero
 - genero un numero casuale -->
 
-# TRACCIA 2
+<!-- # TRACCIA 2
 
 Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
+## SVOLGIMENTO
+
 - chiedere una parola
 - chiedere un'altra parola
-- se le due parole sono uguali stampo entrambe
-- altrimenti se la prima parola è più lunga stampo la prima
-- altrimenti la seconda parola è più lunga stampo la seconda
+- **SE** le due parole sono uguali stampo entrambe
+- **ALTRIMENTI** **SE** la prima parola è più lunga stampo la prima
+- **ALTRIMENTI** la seconda parola è più lunga stampo la seconda -->
+<!--
+# TRACCIA 3
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+## SVOLGIMENTO
+
+- creo un array vuoto
+- creo la variabile per contenere la somma
+- chiedo altri numeri all'utente
+- chiedo all'utente i numeri
+- inseriamo nell'array i numeri -->
+
+<!-- # TRACCIA 4
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+Calcola la media dei numeri inseriti.
+
+## SVOLGIMENTO
+
+- creo un array vuoto
+- creo la variabile per contenere la somma
+- chiedo altri numeri all'utente
+- chiedo all'utente i numeri
+- facciamo la media
+- inseriamo nell'array i numeri -->
--- main.js
+++ main.js
@@ -8,27 +8,88 @@
 // if (firstNumber < secondNumber) {
 //   let NumberCompreso =
 //     Math.floor(Math.random() * (firstNumber - secondNumber + 1)) + firstNumber;
+// } else {
+//   let NumberCompreso =
+//     Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + secondNumber;
 // }
 
 // console.log(NumberCompreso);
 
-//* chiedo all'utente una parola
-const primaParola = prompt("Inserisci una parola");
+// //* chiedo all'utente una parola
+// const primaParola = prompt("Inserisci una parola");
 
-//* chiedo all'utente un'altra parola
-const secondaParola = prompt("Inserisci un'altra parola");
+// //* chiedo all'utente un'altra parola
+// const secondaParola = prompt("Inserisci un'altra parola");
 
-//* se le due parole sono uguali stampo entrambe
-if (primaParola.length == secondaParola.length) {
-  console.log(primaParola, secondaParola);
-}
+// //* se le due parole sono uguali stampo entrambe
+// if (primaParola.length == secondaParola.length) {
+//   console.log(primaParola, secondaParola);
+// }
 
-//* altrimenti se la prima parola è più lunga stampo la prima
-else if (primaParola.length > secondaParola.length) {
-  console.log(primaParola);
-}
+// //* altrimenti se la prima parola è più lunga stampo la prima
+// else if (primaParola.length > secondaParola.length) {
+//   console.log(primaParola);
+// }
 
-//* altrimenti la seconda parola è più lunga stampo la seconda
-else {
-  console.log(secondaParola);
-}
+// //* altrimenti la seconda parola è più lunga stampo la seconda
+// else {
+//   console.log(secondaParola);
+// }
+
+// //* creo un array vuoto
+// const numbers = [];
+
+// //* creo la variabile per contenere la somma
+// let numbersAdd = 0;
+
+// //* chiedo altri numeri all'utente
+// while (numbersAdd < 50) {
+//   //* chiedo all'utente i numeri
+//   let userNumber = parseInt(prompt("inserisci i tuoi numeri"));
+
+//   numbersAdd += userNumber;
+
+//   numbers.push(userNumber);
+// }
+
+// //* inseriamo nell'array i numeri
+
+// console.log(numbers);
+
+// //* creo un array vuoto
+// const numbers = [];
+
+// //* creo la variabile per contenere la somma
+// let numbersAdd = 0;
+
+// //* chiedo altri numeri all'utente
+// while (numbersAdd < 50) {
+//   //* chiedo all'utente i numeri
+//   let userNumber = parseInt(prompt("inserisci i tuoi numeri"));
+
+//   numbersAdd += userNumber;
+
+//   numbers.push(userNumber);
+
+//   //* facciamo la media
+//   let media = numbersAdd / numbers.length;
+
+//   console.log(media);
+// }
+
+// //* inseriamo nell'array i numeri
+
+// console.log(numbers);
+
+//* Trovare gli errori nel seguente codice:
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// let evenNumbers = [];
+// for (let i = 0; i < numbers.length - 1; i++) {
+//   if (numbers[i] % 2 == 0) {
+//     evenNumbers.push(i);
+//   }
+// }
+// console.log(evenNumbers);
+
+// // dovrebbe restituire [1,3,5]
